f366819420781b6f50cb421d90d1cddc1807e69a
angelusa c.de albuquerque sá barreto c. de albuquerque <anapaulasapaiva@gmail.com>
2024-07-08 14:14:34 -0300

    nao esta certo

--- entrega2/server.py
+++ entrega2/server.py
@@ -2,6 +2,7 @@ import socket as skt    #importando as bibliotecas
 import os
 from time import sleep
 import math
+from rdt3 import *
 
 #Setando as informações do socket
 ip = 'localhost' # IP do sevidor
@@ -14,6 +15,8 @@ server_socket.bind(server_addr) # Configurando o socket do servidor
 buffer_size = 1024 # Tamanho maximo do buffer
 print('The server is ready to receive')
 
+# Numero de sequencia para rdt3.0
+seq_number = 0
 
 # Definindo o retorno dos pacotes
 
@@ -29,36 +32,38 @@ def send_file(file_path1):
             server_socket.sendto(file_data,client_addr)
             print(f"Package {i+1}/{package_number} sent")
 
-while 1:
+try:
+    while 1:
 
-    # Como primeiro enviamos o path do nosso arquivo, recebemos ele com recvfrom
-    encoded_name , addr = server_socket.recvfrom(buffer_size)
+        # Como primeiro enviamos o path do nosso arquivo, recebemos ele com recvfrom
+        # encoded_name , addr = server_socket.recvfrom(buffer_size)
+        encoded_name, seq_number = rdt_rcv(server_socket, seq_number)
 
-    # Passando o valor em bytes para string novamente
-    name, tipo = (encoded_name.decode().split('\\')[-1]).split('.') # Separamos o nome do arquivo e o seu tipo
-    file_path = name + '.' + tipo
+        # Passando o valor em bytes para string novamente
+        name, tipo = (encoded_name.decode().split('\\')[-1]).split('.') # Separamos o nome do arquivo e o seu tipo
+        file_path = name + '.' + tipo
 
-    #Recebendo o numero de pacotes 
-    data, client_addr = server_socket.recvfrom(buffer_size)
-    package_number = int(data.decode()) # Fazendo um casting para inteiro
+        #Recebendo o numero de pacotes 
+        # data, client_addr = server_socket.recvfrom(buffer_size)
+        data, seq_number = rdt_rcv(server_socket, seq_number)
+        package_number = int(data.decode())
 
-    # Recebendo o arquivo
-    with open(file_path, 'wb') as new_file:
-        try:
-            server_socket.settimeout(10)  # Definindo timeout para 10 segundos
-            for i in range(package_number):
-                package_data, client_addr = server_socket.recvfrom(buffer_size)  # Recebendo pacotes
-                new_file.write(package_data)
-                print(f"Package {i+1}/{package_number} received")
-            server_socket.settimeout(None)  # Desativando o timeout após a recepção
-        except TimeoutError:
-            print('Timeout Error: File reception incomplete')
-    
-    print(f'File received and saved as {file_path}')
-    send_file(file_path)
+        # Recebendo o arquivo
+        with open(file_path, 'wb') as new_file:
+            try:
+                server_socket.settimeout(10)  # Definindo timeout para 10 segundos
+                for i in range(package_number):
+                    # package_data, client_addr = server_socket.recvfrom(buffer_size)  # Recebendo pacotes
+                    package_data, seq_number = rdt_rcv(server_socket, seq_number)
+                    new_file.write(package_data)
+                    print(f"Package {i+1}/{package_number} received")
+                server_socket.settimeout(None)  # Desativando o timeout após a recepção
+            except TimeoutError:
+                print('Timeout Error: File reception incomplete')
+        
+        print(f'File received and saved as {file_path}')
+        send_file(file_path)
 
-    #tentando fazer o servidor fechar
-    # except KeyboardInterrupt:
-    #     server_socket.close()
-
-    
+except KeyboardInterrupt:
+    server_socket.close()
+    print('Server closed')
